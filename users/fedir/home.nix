774d235c59c56ebf13c43856a67fdfeb5990245f
Fedir Nepyivoda <fednep@gmail.com>
2022-10-13 21:36:29 +0300

    Small updates to fish theme settings to make terminal a bit convenient

--- users/fedir/home.nix
+++ users/fedir/home.nix
@@ -44,6 +44,15 @@
   programs.fish = {
     enable = true;
 
+    # Theme settings
+    interactiveShellInit = ''
+      set -g theme_newline_cursor yes
+      set -g fish_prompt_pwd_dir_length 0
+      set -g theme_color_scheme zenburn
+      set -g theme_newline_prompt '$ '
+      fish_vi_key_bindings
+      '';
+
     plugins = [
       {
         name="foreign-env";
