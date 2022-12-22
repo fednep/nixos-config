bae17cb415677bf7d4e4a2cf58172920a616d51d
Fedir Nepyivoda <fednep@gmail.com>
2022-12-22 15:56:26 +0200

    Adds "gcc" package to dev template

--- system/templates/dev.nix
+++ system/templates/dev.nix
@@ -100,6 +100,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    gcc
     gnumake
     killall
     rxvt-unicode-unwrapped
